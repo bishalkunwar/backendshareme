f94ce9516dab48c98cb61fb10aa5b73ce847d536
Bishal487 <bishal.kunwar487@gmail.com>
2024-03-05 16:25:39 -0500

    creating new sanity base

--- schemaTypes/index.js
+++ schemaTypes/index.js
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import User from "./User";
+import Comment from "./Comment";
+import Pin from "./Pin";
+import PostedBy from "./PostedBy";
+import Save from "./Save";
+
+export const schemaTypes = [User, Comment, Pin, PostedBy, Save];
